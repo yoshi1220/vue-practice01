c44a0668104e96ed7777a4211d7b71fb589726d0
yoshi1220 <temari@themis.ocn.ne.jp>
2018-11-23 20:22:28 +0900

    コンポーネントの追加
    Editor, Home

---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="home">
+        <h1>{{ msg }}</h1>
+        <button>Google アカウントでログイン</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "home",
+    data() {
+        return {
+            msg: "Welcome to MyMarkdown"
+        };
+    }
+};
+</script>
