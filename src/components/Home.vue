a869869d22cddb4b92028e5ffa1e1053f1e7c8e0
yoshi1220 <temari@themis.ocn.ne.jp>
2018-11-23 21:39:02 +0900

    FirebaseのGoogle認証機能を追加

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="home">
     <h1>{{ msg }}</h1>
-    <button>Google アカウントでログイン</button>
+    <button @click="googleLogin">Google アカウントでログイン</button>
   </div>
 </template>
 
@@ -12,6 +12,11 @@ export default {
     return {
       msg: "Welcome to MyMarkdown"
     };
+  },
+  methods: {
+    googleLogin: function() {
+      firebase.auth().signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+    }
   }
 };
 </script>
