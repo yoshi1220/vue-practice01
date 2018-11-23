848953afd21d0ef8ad45c23614b192686d9f5f39
yoshi1220 <temari@themis.ocn.ne.jp>
2018-11-23 20:51:37 +0900

    App.vueの変更
    Home, Editor componentsの追加

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,14 +1,14 @@
 <template>
-    <div class="editor">
-        <h1>エディター画面</h1>
-    </div>
+  <div class="editor">
+    <h1>エディター画面</h1>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "editor",
-    data() {
-        return {};
-    }
+  name: "editor",
+  data() {
+    return {};
+  }
 };
 </script>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,17 +1,17 @@
 <template>
-    <div id="home">
-        <h1>{{ msg }}</h1>
-        <button>Google アカウントでログイン</button>
-    </div>
+  <div id="home">
+    <h1>{{ msg }}</h1>
+    <button>Google アカウントでログイン</button>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "home",
-    data() {
-        return {
-            msg: "Welcome to MyMarkdown"
-        };
-    }
+  name: "home",
+  data() {
+    return {
+      msg: "Welcome to MyMarkdown"
+    };
+  }
 };
 </script>
